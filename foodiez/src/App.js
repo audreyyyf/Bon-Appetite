1ecc3e8e6f77c9a3cac99b0f2c490a7d08debc4e
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-13 18:23:05 -0400

    more

--- foodiez/src/App.js
+++ foodiez/src/App.js
@@ -4,12 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
       <h> Foodiez </h>
 
-=======
-<<<<<<< HEAD
-=======
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -24,8 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
->>>>>>> 6a5302fe9f84120ef4008efe47f66f6bde914ace
->>>>>>> 5edf9817caf59996fbfb09c8b27349891d61d66d
+
     </div>
   );
 }
