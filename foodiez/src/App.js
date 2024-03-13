689417ea5401a815c05016d5acc3b867e8ee8211
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-13 18:19:09 -0400

    headings

--- foodiez/src/App.js
+++ foodiez/src/App.js
@@ -4,23 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 6a5302fe9f84120ef4008efe47f66f6bde914ace
+      <h> Foodiez </h>
+
     </div>
   );
 }
---
+++ foodiez/src/Components/Inputs.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="InputPanel">
+      <div className="InputLabel">
+        Ingredients you want:
+      </div>
+      <div className="InputLabel">
+        Ingredients you can't have:
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
