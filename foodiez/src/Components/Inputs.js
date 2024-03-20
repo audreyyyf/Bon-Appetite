357687ce1ea30ce8e03cae7b446db971cc9e6bd4
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-20 11:52:50 -0400

    :|

--- foodiez/src/Components/Inputs.js
+++ foodiez/src/Components/Inputs.js
@@ -1,4 +1,6 @@
+
 import React, { useState } from 'react';
+import '../App.css'; // Import the CSS file
 
 function Inputs() {
   const [inputs1, setInputs1] = useState(['']);
@@ -39,40 +41,44 @@ function Inputs() {
 
   return (
     <div className="InputPanel">
-      <div className="InputLabel">
-        <h2>Ingredients you want:</h2>
+      <div className="InputSection1">
+        <div className="InputLabel">
+          <h2>Ingredients you want:</h2>
+        </div>
+        <div className="InputBoxes">
+          {inputs1.map((input, index) => (
+            <div key={index}>
+              <input className="input"
+                type="text"
+                value={input}
+                onChange={event => handleChange1(index, event)}
+              />
+              <button onClick={() => removeInput1(index)}>Remove</button>
+            </div>
+          ))}
+          <button  onClick={addInput1}>Add Input</button>
+        </div>
       </div>
-      <div className="InputBoxes">
-        {inputs1.map((input, index) => (
-          <div key={index}>
-            <input className="input"
-              type="text"
-              value={input}
-              onChange={event => handleChange1(index, event)}
-            />
-            <button onClick={() => removeInput1(index)}>Remove</button>
-          </div>
-        ))}
-        <button  onClick={addInput1}>Add Input</button>
-      </div>
-      <div className="InputLabel">
-        <h2>Ingredients you can't have:</h2>
-      </div>
-      <div className="InputBoxes">
-        {inputs2.map((input, index) => (
-          <div key={index}>
-            <input className="input"
-              type="text"
-              value={input}
-              onChange={event => handleChange2(index, event)}
-            />
-            <button onClick={() => removeInput2(index)}>Remove</button>
-          </div>
-        ))}
-        <button  onClick={addInput2}>Add Input</button>
-      </div>
-      <div className="InputLabel">
-          <h2> How much time you have: </h2>
+      <div className="InputSection2">
+        <div className="InputLabel">
+          <h2>Ingredients you can't have:</h2>
+        </div>
+        <div className="InputBoxes">
+          {inputs2.map((input, index) => (
+            <div key={index}>
+              <input className="input"
+                type="text"
+                value={input}
+                onChange={event => handleChange2(index, event)}
+              />
+              <button onClick={() => removeInput2(index)}>Remove</button>
+            </div>
+          ))}
+          <button  onClick={addInput2}>Add Input</button>
+        </div>
+        <div className="InputLabel">
+            <h2> How much time you have: </h2>
+        </div>
       </div>
       <div class="InputSliders">
         <input type="range" min="1" max="100"  class="slider" id="slider1" style={{ backgroundColor: 'black', border: '1px solid #4CAF50' }} />
